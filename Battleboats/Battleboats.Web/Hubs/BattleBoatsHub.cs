26235545866dbb447325e5e560d27374fa414253
David E. Manske <davidemanske@gmail.com>
2014-09-19 16:16:45 -0500

    Added CreateGame, JoinGame, and FireShot actions on the hub.

--- Battleboats/Battleboats.Web/Hubs/BattleBoatsHub.cs
+++ Battleboats/Battleboats.Web/Hubs/BattleBoatsHub.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Battleboats.Domain;
+using Battleboats.Infrastructure.Repositories;
+using Battleboats.Web.Models;
 using Microsoft.AspNet.SignalR;
 
 namespace Battleboats.Web.Hubs
 {
     public class BattleBoatsHub : Hub
     {
-        public BattleBoatsHub()
+        private readonly IGameService _gameService;
+        private Game _game;
+
+        public BattleBoatsHub(IGameService gameService)
         {
-            
+            _gameService = gameService;
         }
 
         /// <summary>
@@ -32,8 +38,27 @@ namespace Battleboats.Web.Hubs
             Clients.Caller.JoinResponse(userId);
         }
 
+        public void CreateGame(Guid userId)
+        {
+            _game = new Game(new Player() { IsPlayerTurn = true, Name = "Player 1", PlayerId = userId });
+            Clients.Caller.CreateGameResponse(_game.GameId);
+        }
 
+        public void JoinGame(Guid userId)
+        {
+            _game.PlayerTwo = new Player()  { IsPlayerTurn = false, Name = "Player 2", PlayerId = userId };
+            Clients.Caller.JoinGameResponse(_game.GameId);
+        }
 
+        public void FireShot(Guid userId, Coordinate fire) //  Guid gameId -- Assumed only one game right now
+        {
+            _game.FireShot(userId, fire);
+        }
+
+        public void SetupShip(Guid userId, Guid gameId)
+        {
+            
+        }
 
     }
 }
